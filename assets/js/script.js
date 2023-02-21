3c73d7c7d4fcee7e79f317ed0472565e20fac7ec
Irfnmdlptra <bangasrat@gmail.com>
2023-02-21 17:56:22 +0800

    portfolio

--- assets/js/script.js
+++ assets/js/script.js
@@ -102,3 +102,24 @@ const item = document.querySelectorAll('.cart-item');
 
 
 
+var sections = document.querySelectorAll("section");
+var navLinks = document.querySelectorAll(".navbar a");
+
+window.addEventListener("scroll", function() {
+  var current = "";
+
+  sections.forEach(function(section) {
+    var sectionTop = section.offsetTop;
+    var sectionHeight = section.clientHeight;
+    if (pageYOffset >= sectionTop - sectionHeight / 3) {
+      current = section.getAttribute("id");
+    }
+  });
+
+  navLinks.forEach(function(link) {
+    link.classList.remove("active");
+    if (link.getAttribute("href").slice(1) == current) {
+      link.classList.add("active");
+    }
+  });
+});
